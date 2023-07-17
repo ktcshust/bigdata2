94104328fb2fd9939c8052fa8ac5a51b3a604bfd
ktcshust <kt209stark@gmail.com>
2023-07-17 19:03:41 +0700

    abc

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import pandas as pd
 # Đường dẫn đến tệp serviceAccountKey.json
 cred = credentials.Certificate('bigdata-eec24-firebase-adminsdk-n4a82-94ef80532e.json')
 
-# Khởi tạo ứng dụng Firebase
-firebase_admin.initialize_app(cred)
-
+if not firebase_admin._apps:
+    # Khởi tạo ứng dụng Firebase
+    firebase_admin.initialize_app(cred)
 
 df = pd.read_csv('top_20_ratio.csv')
 
@@ -20,6 +20,11 @@ db = firestore.client()
 collection_ref = db.collection('top_20_ratio')
 
 for record in data:
-    collection_ref.add(record)
-#
+    # Tìm tất cả các tài liệu trong collection có trường 'name' trùng với 'name' từ CSV
+    query = collection_ref.where('name', '==', record['name'])
+    docs = query.stream()
+
+    # Cập nhật trường 'count' của các tài liệu tìm thấy
+    for doc in docs:
+        doc.reference.update({'pledged_goal_ratio ': record['pledged_goal_ratio']})
 
