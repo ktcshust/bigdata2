20fa756e5488702f37c53c4d405283618a6faaf8
ktcshust <kt209stark@gmail.com>
2023-07-17 20:56:08 +0700

    abc

--- main.py
+++ main.py
@@ -28,6 +28,27 @@ for record in data1:
     for doc in docs1:
         doc.reference.update({'pledged_goal_ratio ': record['pledged_goal_ratio']})
 
+existing_names1 = set()
+docs4 = collection_ref1.stream()
+for doc in docs4:
+    existing_names1.add(doc.get('name'))
+docs_to_delete = []
+for name in existing_names1:
+    if name not in [record['name'] for record in data1]:
+        query = collection_ref1.where('name', '==', name)
+        docs = query.stream()
+        for doc in docs:
+            docs_to_delete.append(doc.reference)
+
+# Xóa các tài liệu không cần thiết
+for doc_ref in docs_to_delete:
+    doc_ref.delete()
+
+# Thêm các tài liệu mới cho những 'record[name]' chưa tồn tại trong Firestore
+for record in data1:
+    if record['name'] not in existing_names1:
+        collection_ref1.add(record)
+
 df2 = pd.read_csv('top_20_country.csv')
 
 # Đường dẫn đến tệp CSV
@@ -43,6 +64,27 @@ for record in data2:
     # Cập nhật trường 'count' của các tài liệu tìm thấy
     for doc in docs2:
         doc.reference.update({'count': record['count']})
+
+existing_names2 = set()
+docs5 = collection_ref2.stream()
+for doc in docs5:
+    existing_names2.add(doc.get('name'))
+docs_to_delete = []
+for name in existing_names2:
+    if name not in [record['name'] for record in data2]:
+        query = collection_ref2.where('name', '==', name)
+        docs = query.stream()
+        for doc in docs:
+            docs_to_delete.append(doc.reference)
+
+# Xóa các tài liệu không cần thiết
+for doc_ref in docs_to_delete:
+    doc_ref.delete()
+
+# Thêm các tài liệu mới cho những 'record[name]' chưa tồn tại trong Firestore
+for record in data2:
+    if record['name'] not in existing_names2:
+        collection_ref2.add(record)
 
 df3 = pd.read_csv('top_20_category.csv')
 
@@ -60,4 +102,25 @@ for record in data3:
     # Cập nhật trường 'count' của các tài liệu tìm thấy
     for doc in docs3:
         doc.reference.update({'pledged': record['pledged']})
+
+existing_names3 = set()
+docs6 = collection_ref3.stream()
+for doc in docs6:
+    existing_names3.add(doc.get('name'))
+docs_to_delete = []
+for name in existing_names3:
+    if name not in [record['name'] for record in data3]:
+        query = collection_ref3.where('name', '==', name)
+        docs = query.stream()
+        for doc in docs:
+            docs_to_delete.append(doc.reference)
+
+# Xóa các tài liệu không cần thiết
+for doc_ref in docs_to_delete:
+    doc_ref.delete()
+
+# Thêm các tài liệu mới cho những 'record[name]' chưa tồn tại trong Firestore
+for record in data3:
+    if record['name'] not in existing_names3:
+        collection_ref3.add(record)
 
